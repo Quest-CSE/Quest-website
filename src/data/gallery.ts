9227832b1701783083cc00d8777092a549e2014c
Mukesh-15 <mukeshdoodapaka@gmail.com>
2026-03-01 21:47:19 +0530

    gallery change

--- src/data/gallery.ts
+++ src/data/gallery.ts
@@ -32,6 +32,7 @@ export const gallerySections: GallerySection[] = [
       "https://res.cloudinary.com/dvvijlfio/image/upload/v1772022731/DSC_0534_mzdgfk.jpg",
       "https://res.cloudinary.com/dvvijlfio/image/upload/v1771934106/efew_vd9ykb.jpg",
       "https://res.cloudinary.com/dvvijlfio/image/upload/v1772041984/class_mvqfio.jpg",
+      "https://res.cloudinary.com/dvvijlfio/image/upload/v1772381772/IMG-20260301-WA0020.jpg_r7dyxj.jpg",
       "https://res.cloudinary.com/dvvijlfio/image/upload/v1772042675/clg_2022_bwlbvm.png",
       "https://res.cloudinary.com/dvvijlfio/image/upload/v1772096374/event_photo_past_3_wnuxqz.jpg",
       "https://res.cloudinary.com/dvvijlfio/image/upload/v1772096390/event_photo_past_1_wzv6or.png",
