0b4588fabad5711a95a1389b63bc150535d3a85e
Quest-CSE <quest.cse@jntuh.ac.in>
2026-03-25 15:13:06 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -26,7 +26,7 @@ const Navbar = () => {
           </div>
           <div className="w-40 h-30 rounded-lg  flex items-center justify-center">
             {/* <span className="text-primary-foreground font-display font-bold text-sm"></span> */}
-            <img src = "https://res.cloudinary.com/dvvijlfio/image/upload/v1773639927/Quest_2026_Logo_Final-removebg-preview_2_wommof.png"/>
+            <img src = "https://res.cloudinary.com/dvvijlfio/image/upload/v1774431751/Your_paragraph_text__1_-removebg-preview_mimehj.png"/>
           </div>
           
         </Link>
